554c26d43d0167c76e2e9c610b49290c23fd0c42
Xeeija <19950592+xeeija@users.noreply.github.com>
2021-03-27 15:32:00 +0100

    Update wheel, add basic style

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Counter } from './components/Counter';
 import { LoginForm } from './components/LoginForm';
-import { PrizeWheel } from './components/RandomWheel';
+import { RandomWheel } from './components/RandomWheel';
 import { RegisterForm } from './components/RegisterForm';
+import "./style.css"
 
 const App: React.FC = () => {
+
+  const [names, setNames] = useState<string[]>([])
+
   return (
     <div className="App">
-      Hello React
-      <Counter>
-        {(count, setCount) => (
-          <div>
-            <div>{count}</div>
-            <button onClick={() => setCount(count + 1)}>Click</button>
-          </div>
-        )}
-
-      </Counter>
+      <div>
+        <p>Hello React</p>
+        <Counter>
+          {(count, setCount) => (
+            <div>
+              <div>{count}</div>
+              <button onClick={() => {
+                setCount(count + 1)
+                const wheel = document.querySelector("#wheel-g")
+                wheel?.classList.add("spinning")
+                setTimeout(() => {
+                  wheel?.classList.remove("spinning")
+                }, 4000);
+              }}>Click</button>
+            </div>
+          )}
+        </Counter>
+      </div>
 
       <br />
       <div>Register:</div>
@@ -27,7 +39,17 @@ const App: React.FC = () => {
       <LoginForm />
 
       <br />
-      <PrizeWheel segments={7} />
+      <RandomWheel diameter={600} names={names} ></RandomWheel>
+
+      <textarea
+        style={{ height: 400, width: 200, marginLeft: 20 }}
+        value={names.join("\n")}
+        onChange={(e) => {
+          console.log(e.target.value)
+          setNames(e.target.value.split("\n"))
+        }}>
+      </textarea>
+
     </div>
   );
 }
--- app/src/components/RandomWheel.tsx
+++ app/src/components/RandomWheel.tsx
@@ -1,7 +1,11 @@
-import React from "react"
+import React, { useRef } from "react"
+import "../style.css"
 
 interface Props {
-  segments: number
+  // segments: number,
+  names?: string[],
+  diameter: number,
+  // colors?: string[]
 }
 
 interface Point {
@@ -26,72 +30,99 @@ const pointOnCircle = (center: Point, radius: number, angleDeg: number, angleOff
   }
 }
 
-export const PrizeWheel: React.FC<Props> = ({ segments }) => {
+export const RandomWheel: React.FC<Props> = ({ diameter, names = [] } /* { segments } */) => {
+
+  // Remove empty lines
+  names = names.filter(x => x)
+
+  const wheelRef = useRef<SVGGElement>(null)
+
+  // wheelRef.current?.classList.
+
+  // const [names, setNames] = useState<string[]>([""])
+  const segments = names.length
 
   const d: Sector = {
-    center: { x: 200, y: 200 },
-    radius: 195,
+    center: { x: diameter / 2, y: diameter / 2 },
+    radius: (diameter / 2) - 5,
     startAngle: 0,
-    endAngle: segments !== 1 ? 360 / segments : 359.99
+    endAngle: segments > 1 ? 360 / segments : 360 // 360° angle would be the same as 0°
   }
 
   const largeArcFlag = d.endAngle - d.startAngle <= 180 ? 0 : 1
-
-  // const points = [...Array(segments).keys()].map((_, i) => {
-  //   return pointOnCircle(d.center, d.radius, d.startAngle, i * d.endAngle)
-  // })
-  // console.log(points)
 
   const colors = [
     "orchid",
     "lightgreen",
     "aquamarine",
     "tomato",
-    "lightblue",
+    "cyan",
     "orange",
   ]
-
-  // const startPos = polar(d.center, d.radius, d.endAngle)
-  // const endPos = polar(d.center, d.radius, d.startAngle)
-
-  // console.log(startPos)
-  // console.log(endPos)
 
   /* M   200       0         A   200     200     0 1        0 400     200     L    200  200  Z
     Move startPosX startPosY Arc radiusX radiusY ? largeArc ? arcPosX arcPosY Line posX posY Close path
     arcPos -> creates an arc from current position (like a cursor) to given arcPos */
   return (
-    <svg height="400" width="400">
-      <circle cx={d.center.x} cy={d.center.y} r={d.radius} stroke="#222" strokeWidth="5" fill="none" />
+    <svg {...{
+      // id: "random-wheel",
+      // className: "spinning",
+      height: diameter,
+      width: diameter,
+    }} >
+      <g id="wheel-g" ref={wheelRef} className="" >
+        <circle cx={d.center.x} cy={d.center.y} r={d.radius} stroke="#222" strokeWidth="5" fill={colors[colors.length / 2]} />
 
-      {[...Array(segments).keys()].map((_, i) => {
-        const startPos = pointOnCircle(d.center, d.radius, d.endAngle, i * d.endAngle)
-        const endPos = pointOnCircle(d.center, d.radius, d.startAngle, i * d.endAngle)
+        {names.map((name, i) => {
+          const startPos = pointOnCircle(d.center, d.radius, d.endAngle, i * d.endAngle)
+          const endPos = pointOnCircle(d.center, d.radius, d.startAngle, i * d.endAngle)
+          const middlePos = pointOnCircle(d.center, d.radius, (d.endAngle / 2) + 1, i * d.endAngle) // for text path
 
-        // replace with different color, if last color is the same as the first
-        const colorIndex = i % colors.length
-        const color = (colorIndex === 0 && i === segments - 1) ? colors[colors.length / 2] : colors[colorIndex]
-        // const color = colors[i % colors.length]
+          // replace with different color, if last color is the same as the first
+          const colorIndex = i % colors.length
+          const color = (colorIndex === 0 && i === segments - 1) ? colors[colors.length / 2] : colors[colorIndex]
+          // const color = colors[i % colorlength]
 
-        return (
-          <path stroke="#222" strokeWidth="1" fillOpacity="0.5" fill={color} d={
-            `M ${startPos.x} ${startPos.y} ` +
-            `A ${d.radius} ${d.radius} 0 ${largeArcFlag} 0 ${endPos.x} ${endPos.y} ` +
-            `L ${d.center.x} ${d.center.y} Z`
-          } />
-        )
-      })}
+          return (
+            <g key={"wheel-g-" + i}>
+              <path
+                className="wheel-sector"
+                fill={color}
+                d={
+                  `M ${startPos.x} ${startPos.y} ` +
+                  `A ${d.radius} ${d.radius} 0 ${largeArcFlag} 0 ${endPos.x} ${endPos.y} ` +
+                  `L ${d.center.x} ${d.center.y} Z`
+                } />
+              <defs>
+                <path
+                  id={"wheel-text-path-" + i}
+                  d={`M ${d.center.x} ${d.center.y} L ${middlePos.x} ${middlePos.y} `}
+                />
+              </defs>
+              <text color={color} >
+                <textPath
+                  // width attribute zeigt die Textlänge an
+                  className="wheel-text"
+                  textAnchor="end"
+                  dominantBaseline="middle"
+                  startOffset="92%"
+                  xlinkHref={"#wheel-text-path-" + i} >
+                  {name}
+                </textPath>
+              </text>
+              {/* <circle cx={middlePos.x} cy={middlePos.y} r="5" fill={color} /> */}
+            </g>
+          )
+        })}
 
-      {/* <path stroke="purple" strokeWidth="2" fill="purple" d={
-        `M ${startPos.x} ${startPos.y} ` +
-        `A ${d.radius} ${d.radius} 0 ${largeArcFlag} 0 ${endPos.x} ${endPos.y} ` +
-        `L ${d.center.x} ${d.center.y} Z`
-      } /> */}
+        {/* Middle circle */}
+        <circle cx={d.center.x} cy={d.center.y} r={d.radius * 0.1} fill="#333" stroke="#222" strokeWidth="3" />
 
-      {/* <circle cx={startPos.x} cy={startPos.y} r="5" />
+        {/* <circle cx={startPos.x} cy={startPos.y} r="5" />
       <circle cx={endPos.x} cy={endPos.y} r="5" /> */}
+        {/* {points.map((v, i) => <circle cx={v.x} cy={v.y} r="5" fill={colors[i % colors.length]} key={"segment" + i} />)} */}
 
-      {/* {points.map((v, i) => <circle cx={v.x} cy={v.y} r="5" fill={colors[i % colors.length]} key={"segment" + i} />)} */}
+      </g>
     </svg>
   )
 }
